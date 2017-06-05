72f7550781a3d223f7a88a4bc68588081c2be548
Joseph Pong <oxjoe2006@yahoo.com>
2017-06-05 18:44:39 -0400

    init commit

---
+++ fizzBuzzSolution.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Joseph on 6/5/2017.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class fizzBuzzSolution {
+
+  public static void main(String[] args) {
+    fizzBuzzSolution obj = new fizzBuzzSolution();
+    System.out.println(obj.fizzBuzz(15));
+  }
+
+  public List<String> fizzBuzz(int n) {
+    // init list
+    List<String> myList = new ArrayList<String>();
+
+    // iterate up to n
+    for (int i = 1; i <= n; i++) {
+      if (i % 15 == 0) { // multiple of 15, add FizzBuzz. Has to be first so it wont just add Fizz
+        myList.add("FizzBuzz");
+      } else if (i % 3 == 0) { // multiple of 3, add Fizz
+        myList.add("Fizz");
+      } else if (i % 5 == 0) { // multiple of 5, add Buzz
+        myList.add("Buzz");
+      } else { // add number but concatenate
+        myList.add("" + i);
+      }
+    }
+
+    return myList;
+  }
+}
+
+  /* OUTPUT
+  [1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz]
+   */
