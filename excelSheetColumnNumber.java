431dbdd694a4aa7763ff2bd35acc2209dda48602
Joseph Pong <oxjoe2006@yahoo.com>
2017-06-08 22:03:59 -0400

    init commit

---
+++ excelSheetColumnNumber.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Joseph on 6/8/2017.
+ * https://leetcode.com/problems/excel-sheet-column-number/#/description
+ */
+public class excelSheetColumnNumber {
+
+  /*
+  I/O:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+   */
+
+  /*
+  Make a dictionary for A-Z and and compare each key to string entered
+  if String has two chars then get the second char, compare it to key and add 26.
+   */
+
+  public int titleToNumber(String s) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
