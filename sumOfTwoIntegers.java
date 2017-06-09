d634c24f5b922bd2b2452b9e68370e3483777be2
Joseph Pong <oxjoe2006@yahoo.com>
2017-06-08 21:31:08 -0400

    tried for about 20 min

--- sumOfTwoIntegers.java
+++ sumOfTwoIntegers.java
@@ -5,22 +5,23 @@
 public class sumOfTwoIntegers {
 
   public static int getSum(int a, int b) {
-//    if (a == b) {
-//      return a * 2;
-//
-//    } else if (a == 0 && b != 0) {
-//      return b;
-//    } else if (a != 0 && b == 0) {
-//      return a;
-//    } else if (a == 0 && b == 0) {
-//      return 0;
-//    }
     int max = Math.max(a,b);
     int min = Math.min(a,b);
+
+    if (a == b) {
+      return a * 2;
+    } else if (a == 0 && b != 0) {
+      return b;
+    } else if (a != 0 && b == 0) {
+      return a;
+    } else if (a == 0 && b == 0 || min * -1 == max) {
+      return 0;
+    }
+
     for (int i = 0; i < Math.abs(max); i++) {
-      if (min > 0) {
+      if (min > 0 || (min < 0 && Math.abs(min) == min)) {
         min++;
-      } else {
+      } else if (min < 0) {
         min--;
       }
     }
@@ -28,7 +29,7 @@ public class sumOfTwoIntegers {
   }
 
   public static void main(String[] args) {
-    System.out.println(getSum(-1, 1));
+    System.out.println(getSum(-14, 16));
 //    System.out.println(getSum(21, 43));
 //    System.out.println(getSum(0, 0));
 //    System.out.println(getSum(0, 3));
