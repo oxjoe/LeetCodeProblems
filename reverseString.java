77ee11507962ff1c26bd53d2f85300298f2a3660
Joseph Pong <oxjoe2006@yahoo.com>
2017-06-18 14:23:48 -0400

    updated so it makes more sense

--- reverseString.java
+++ reverseString.java
@@ -5,29 +5,32 @@
 public class reverseString {
 
   public static String reverseStringOne(String s) {
+    // Swaps begin and end element, increasing begin and decreasing end each time.
+    if (s.length() == 0 || s == null) {
+      return "";
+    }
+    char[] charArray = s.toCharArray();
+    int begin = 0;
+    int end = s.length() - 1;
+    while (begin <= end) {
+      char c = charArray[begin];
+      charArray[begin] = charArray[end];
+      charArray[end] = c;
+      begin++;
+      end--;
+    }
+    return new String(charArray);
+  }
 
+  public static String reverseStringTwo(String s) {
     return new StringBuilder(s).reverse().toString();
   }
 
-  //  Bottom two methods -> time limit exceeded
-  public static String reverseStringTwo(String s) {
-
-    char[] myArr = s.toCharArray();
-    String answer = "";
-    for (int i = s.length() - 1; i >= 0; i--) {
-      answer += myArr[i];
-    }
-    return answer;
-  }
-
   public static String reverseStringThree(String s) {
-
-    String answer = "";
-    for (int i = s.length() - 1; i >= 0; i--) {
-      answer += s.substring(i, i + 1);
-    }
-    return answer; // substring is (inclusive, exclusive)
+    StringBuilder sb = new StringBuilder(s);
+    return sb.reverse().toString();
   }
+
 
   public static void main(String[] args) {
     System.out.println(reverseStringOne("hello"));
