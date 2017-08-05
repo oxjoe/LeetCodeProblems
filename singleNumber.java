6c8212bad9009a042694ae0510f72ad6b2f5905d
Joseph Pong <oxjoe2006@yahoo.com>
2017-08-05 01:25:43 -0400

    updated w/ xor version

--- singleNumber.java
+++ singleNumber.java
@@ -1,60 +1,43 @@
 /**
- * Created by Joseph on 6/5/2017.
- * https://leetcode.com/problems/single-number/#/description
- * Given an array of integers, every element appears twice except for one. Find that single one.
+ * Created by Joseph on 6/5/2017. https://leetcode.com/problems/single-number/#/description Given an
+ * array of integers, every element appears twice except for one. Find that single one.
  */
 
 import java.util.Arrays;
 
 public class singleNumber {
 
+  //  My way
   public static int solution(int[] nums) {
-
-    // Sort the array
     Arrays.sort(nums);
+    boolean state = true;
+    int i = 0;
     int answer = 0;
 
-//  iterate through array, input will always be odd, i+=2 to skip every two ints
-    for (int i = 0; i <= nums.length + 1; i += 2) {
-//       if cur value is != to cur value + 1 then cur is answer, if cur value + 1 is out of bounds
-//       then goto catch
-      try {
-        if (nums[i] != nums[i + 1]) {
-          answer = nums[i];
-          break;
-        }
-//     Catch the error and set answer as the last element in array
-      } catch (ArrayIndexOutOfBoundsException e) {
-        answer = nums[nums.length - 1];
-        break;
+    while (state) {
+      if (i == nums.length - 1 || nums[i] != nums[i + 1]) {
+        answer = nums[i];
+        state = false;
+      } else if (nums[i] == nums[i + 1]) {
+        i += 2;
       }
     }
     return answer;
   }
 
+  // Other way
+  public static int solutionTwo(int[] nums) {
+    int result = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      result = result ^ nums[i];
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
-    int[] array = new int[]{1, 1, 3, 3, 2, 6, 6, 5, 5};
-    System.out.print(solution(array));
+    int[] a = new int[]{1};
+    int[] b = new int[]{5, 2, 3, 3, 2, 5, 4};
+    System.out.print(solution(a));
+    System.out.print(solution(b));
   }
 }
-
-// Home come this has error of missing return statement? scope of return?
-
-/*
-    // Sort the array
-    Arrays.sort(nums);
-    int answer = 0;
-
-    // Iterate thorough and see if the current value and the next value are the same
-    // if they aren't then the current value is the answer
-    for (int i = 0; i <= nums.length + 1; i += 2) {
-      try {
-        if (nums[i] != nums[i + 1]) {
-          return nums[i];
-        }
-      } catch (ArrayIndexOutOfBoundsException e) {
-        return nums[nums.length - 1];
-      }
-    }
-    //return 0;
-  */
