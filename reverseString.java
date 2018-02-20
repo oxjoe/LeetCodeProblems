55803f11bb78f933d081dd8085a6521a2150288f
Joseph Pong <oxjoe2006@yahoo.com>
2018-02-19 20:38:20 -0500

    updated string

--- reverseString.java
+++ reverseString.java
@@ -1,18 +1,20 @@
 /**
- * Created by Joseph on 6/5/2017.
- * https://leetcode.com/problems/reverse-string/#/description
+ * Created by Joseph on 6/5/2017. https://leetcode.com/problems/reverse-string/
  */
 public class reverseString {
 
+  // Swaps begin and end element, increasing begin and decreasing end each time.
   public static String reverseStringOne(String s) {
-    // Swaps begin and end element, increasing begin and decreasing end each time.
-    if (s.length() == 0 || s == null) {
+
+    // Not necessary
+    if (s.length() == 0) {
       return "";
     }
-    char[] charArray = s.toCharArray();
-    int begin = 0;
-    int end = s.length() - 1;
+    char[] charArray = s.toCharArray(); // Converts string to array
+    int begin = 0; // begin is 0 so it will be the first letter of the string
+    int end = s.length() - 1; // end will be the last letter of the string
     while (begin <= end) {
+      // These 3 lines just swap the first and last letter of the string
       char c = charArray[begin];
       charArray[begin] = charArray[end];
       charArray[end] = c;
@@ -23,7 +25,8 @@ public class reverseString {
   }
 
   public static String reverseStringTwo(String s) {
-    return new StringBuilder(s).reverse().toString();
+    return new StringBuilder(s).reverse()
+        .toString(); // Has to have toString() b/c or else return type will be of type StringBuilder
   }
 
   public static String reverseStringThree(String s) {
@@ -31,8 +34,8 @@ public class reverseString {
     return sb.reverse().toString();
   }
 
-
   public static void main(String[] args) {
+    System.out.println(reverseStringOne(""));
     System.out.println(reverseStringOne("hello"));
     System.out.println(reverseStringTwo("hello"));
     System.out.println(reverseStringThree("hello"));
